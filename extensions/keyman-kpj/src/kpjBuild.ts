2ce9f745576825a26e60e941d8d8bf867052a6c1
Steven R. Loomis <srl295@gmail.com>
2024-11-04 15:50:35 -0600

    chore(kpj): cleanup and restructure

---
+++ extensions/keyman-kpj/src/kpjBuild.ts
@@ -0,0 +1,34 @@
+/*
+ * Keyman is copyright (C) SIL Global. MIT License.
+ */
+
+
+/**
+ * Code related to building KPJ projects
+ */
+
+// import {declareBuild  } from '@keymanapp/kmc/build/src/commands/build.js';
+// import { CompilerOptions, CompilerCallbackOptions } from '@keymanapp/developer-utils';
+// import { NodeCompilerCallbacks } from '@keymanapp/kmc/build/src/util/NodeCompilerCallbacks';
+
+async function buildProject(infile: string): Promise<boolean> {
+    // const outfile = '';
+    // const coptions : CompilerCallbackOptions = {
+    // };
+    // const options : CompilerOptions = {
+    // };
+    // const callbacks = new NodeCompilerCallbacks(coptions);
+
+    // const resp = await (new BuildProject().build(
+    // 	infile,
+    // 	<string><unknown>undefined,
+    // 	callbacks,
+    // 	options
+    // ));
+
+    // // dump all
+    // callbacks.messages?.forEach(m => console.dir(m));
+    const resp = false;
+
+    return resp;
+}
